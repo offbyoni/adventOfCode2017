11d98d77ebb43d6b2775ecc0470830c8f3503da1
Toni Rheker <toni@rheker.net>
2017-12-02 00:22:49 +0100

    green second test

--- captcha.py
+++ captcha.py
@@ -1,3 +1,12 @@
 class captcha():
     def capt(input):
-        return 3
+        in_string = str(input)
+        res = 0
+        for index, obj in enumerate(in_string):
+            if index < len(in_string) - 1:
+                next_ = in_string[index + 1]
+            else:
+                next_ = in_string[0]
+            if obj == next_:
+                res += int(obj)
+        return res
