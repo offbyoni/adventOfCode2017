50968a3d9f5c7ff019664f53f5acc771b592cf96
Toni Rheker <toni@rheker.net>
2017-12-02 00:24:38 +0100

    third test 1234_0

--- tests.py
+++ tests.py
@@ -13,5 +13,10 @@ class advent1Test(unittest.TestCase):
             expectedResult = 4
             self.assertEqual(captcha.capt(input), 4)
 
+      def test_1234_0(self):
+            input = 1234
+            expectedResult = 0
+            self.assertEqual(captcha.capt(input), 0)
+
 if __name__ == '__main__':
    unittest.main()
