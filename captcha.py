236f5bc52cc2a974fbd7d23973a14c2e8d6ea88f
Toni Rheker <toni@rheker.net>
2017-12-01 23:58:11 +0100

    second unit test

--- captcha.py
+++ captcha.py
@@ -1,3 +1,3 @@
 class captcha():
     def capt(input):
-        return 2
+        return 3
--- tests.py
+++ tests.py
@@ -8,5 +8,10 @@ class advent1Test(unittest.TestCase):
             expectedResult = 3
             self.assertEqual(captcha.capt(input), 3);
 
+      def test_1111_4(self):
+            input = 1111
+            expectedResult = 4
+            self.assertEqual(captcha.capt(input), 4)
+
 if __name__ == '__main__':
    unittest.main()
