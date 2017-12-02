59428dc061bdf2af3d5c5385fb2ae9a96abfc0bf
Toni Rheker <toni@rheker.net>
2017-12-02 01:00:18 +0100

    add second test for part 2

--- tests2.py
+++ tests2.py
@@ -7,5 +7,10 @@ class advent1Test2(unittest.TestCase):
             res = captcha2.capt2(input)
             self.assertEqual(res, 6)
 
+      def test_1221_0(self):
+            input = "1221"
+            res = captcha2.capt2(input)
+            self.assertEqual(res, 0)
+
 if __name__ == '__main__':
    unittest.main()
