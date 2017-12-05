221c1801e8ab5902db501a2f01f97a92088d36e4
Toni Rheker <toni@rheker.net>
2017-12-05 21:59:29 +0100

    second test

--- tests.py
+++ tests.py
@@ -2,9 +2,13 @@ import unittest
 from passphraseChecker import passphraseChecker
 
 class advent4Test(unittest.TestCase):
-    def test_CheckInValidphrase(self):
-        passphrase = "aa aa bb"
+    def test_checkInValidphrase(self):
+        passphrase = "aa bb cc dd aa"
         self.assertFalse(passphraseChecker.checkPhraseValid(passphrase))
+
+    def test_checkValidPassphrase(self):
+        passphrase = "aa bb cc dd ee"
+        self.assertTrue(passphraseChecker.checkPhraseValid(passphrase))
 
 if __name__ == '__main__':
     unittest.main()
