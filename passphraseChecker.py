630ad36aa0a91c7987e9674e4e7ebb0e335e4796
Toni Rheker <toni@rheker.net>
2017-12-05 22:09:07 +0100

    compare single rows

--- passphraseChecker.py
+++ passphraseChecker.py
@@ -1,3 +1,8 @@
 class passphraseChecker():
     def checkPhraseValid(passphrase):
-        return False
+        words = passphrase.split(" ")
+        for idx in range(len(words)):
+            for idx2 in range (idx+1, len(words)):
+                if (words[idx] == words[idx2]):
+                    return False
+        return True
