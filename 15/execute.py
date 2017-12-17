9bd12fd803dfb6ce61b87c51fb1623160472dba4
Toni Rheker <toni@rheker.net>
2017-12-17 17:48:06 +0100

    execution number 2

--- 15/execute.py
+++ 15/execute.py
@@ -1,7 +1,10 @@
 from generator import *
 from judge import *
 
-genA = generator(40000000, 512, 16807)
-genB = generator(40000000, 191, 48271)
-print(judge(genA, genB))
+#genA = generator(40000000, 512, 16807)
+#genB = generator(40000000, 191, 48271)
+#print(judge(genA, genB))
 
+genA = generatorMod(5000000, 512, 16807, 4)
+genB = generatorMod(5000000, 191, 48271, 8)
+print(judge(genA, genB))
